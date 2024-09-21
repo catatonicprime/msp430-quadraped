5efbc225ac566e6dd9d10d93e4decf02360aa124
catatonicprime <catatonicprime@gmail.com>
2024-09-21 10:48:45 -0700

    CPU Clock helpers

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <msp430.h>
 
+void CPU16Mhz();
+void CPU1Mhz();
+
 int main() {
+    // Disable the watchdog timer to prevent regular resets.
     WDTCTL = WDTPW + WDTHOLD;
+
+    
+    CPU16Mhz();
+
     P1OUT = 0x00;
     P1DIR = BIT0 | BIT6; //LED1/2 are outputs
     int counter = 0;
@@ -12,4 +20,16 @@ int main() {
         }
     }
     return 0;
+}
+
+void CPU16Mhz()
+{
+  DCOCTL = DCO0 | DCO1;
+  BCSCTL1 = RSEL3 | RSEL2 | RSEL1 | RSEL0;
+}
+
+void CPU1Mhz()
+{
+  DCOCTL = DCO0 | DCO1;
+  BCSCTL1 = RSEL2 | RSEL1 | RSEL0;
 }
